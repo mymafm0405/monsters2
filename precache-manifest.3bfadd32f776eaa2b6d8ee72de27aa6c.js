f2b4d9db0106534d8c8749861167c4854403f4e7
Mahmoud <mymafm0405@gmail.com>
2020-10-10 19:37:26 +0300

    Updates

---
+++ precache-manifest.3bfadd32f776eaa2b6d8ee72de27aa6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78839a96532c05330a10182dbb953d43",
+    "url": "/mymafm0405/monsters2.git/index.html"
+  },
+  {
+    "revision": "938a432cd871db9d927f",
+    "url": "/mymafm0405/monsters2.git/static/css/main.15f7cecc.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/mymafm0405/monsters2.git/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mymafm0405/monsters2.git/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "938a432cd871db9d927f",
+    "url": "/mymafm0405/monsters2.git/static/js/main.65518d78.chunk.js"
+  },
+  {
+    "revision": "ce150a377924fa294695",
+    "url": "/mymafm0405/monsters2.git/static/js/runtime-main.c207e822.js"
+  }
+]);
